e779ae5565fbaec4a0dfafeeab8cb14fc21b9de3
phpdeveloper2497 <php2497@gmail.com>
2024-02-13 15:18:50 +0500

    reply check end 1

--- app/telegram/Handler.php
+++ app/telegram/Handler.php
@@ -35,53 +35,53 @@ class Handler extends WebhookHandler
 
     }
 
-//    public function handleChatMessage(Stringable $text): void
-//    {
-//    $phone = $this->message->contact()->phoneNumber();
-//    $userId = $this->message->contact()->userId();
-//    $verifyUserId = $this->message->from()->id();
-//
-//    $isVerifyPhone = intval($userId ==$verifyUserId);
-//
-//    $this->chat->message("Welcome to Telegram bot!")->removeReplyKeyboard()->send();
-//    }
-//
-//public function about()
-//{
-//    Telegraph::message("Our telegram bot will teach you laravel")->send();
-//}
-//
-//    public function help(): void
-//    {
-//        Telegraph::message("Laravel Bot Support Center. How can I help you?")->send();
-//    }
-//    public function actions(): void
-//    {
-////        dd('kop');
-//        Telegraph::message('Choose what action')
-//            ->keyboard(Keyboard::make()->buttons([
-//                Button::make('go to website')->url('https://defstudio.github.io/telegraph/'),
-//                Button::make('layk qo\'yish')->action('like'),
-//                Button::make('Kanalga a\'zo bo\'sh')
-//                    ->action('subscribe')
-//                    ->param('group_name', 'LearningLaravel'),
-//                Button::make('switch')->switchInlineQuery('foo')->currentChat(),
-//            ]))->send();
-//    }
-//
-//    public function like(): void
-//    {
-////        $chat = TelegraphChat::find(1);
-//        //        $chat->message('hello')->send();
-//
-//
-//        $this->reply("Thank you for your like!");
-//    }
-//
-//    public function subscribe(): void
-//    {
-//        Telegraph::message("Thanks for joining the group {$this->data->get('group_name')}!")->send(); //
-//    }
+    public function handleChatMessage(Stringable $text): void
+    {
+    $phone = $this->message->contact()->phoneNumber();
+    $userId = $this->message->contact()->userId();
+    $verifyUserId = $this->message->from()->id();
+
+    $isVerifyPhone = intval($userId ==$verifyUserId);
+
+    $this->chat->message("Welcome to Telegram bot!")->removeReplyKeyboard()->send();
+    }
+
+public function about()
+{
+    Telegraph::message("Our telegram bot will teach you laravel")->send();
+}
+
+    public function help(): void
+    {
+        Telegraph::message("Laravel Bot Support Center. How can I help you?")->send();
+    }
+    public function actions(): void
+    {
+//        dd('kop');
+        Telegraph::message('Choose what action')
+            ->keyboard(Keyboard::make()->buttons([
+                Button::make('go to website')->url('https://defstudio.github.io/telegraph/'),
+                Button::make('layk qo\'yish')->action('like'),
+                Button::make('Kanalga a\'zo bo\'sh')
+                    ->action('subscribe')
+                    ->param('group_name', 'LearningLaravel'),
+                Button::make('switch')->switchInlineQuery('foo')->currentChat(),
+            ]))->send();
+    }
+
+    public function like(): void
+    {
+//        $chat = TelegraphChat::find(1);
+        //        $chat->message('hello')->send();
+
+
+        $this->reply("Thank you for your like!");
+    }
+
+    public function subscribe(): void
+    {
+        Telegraph::message("Thanks for joining the group {$this->data->get('group_name')}!")->send(); //
+    }
 
 
 }
